0333bd57364bec174755be5a715c698733eeea35
Kate Davis <kate.davis887@gmail.com>
2021-06-08 19:07:31 -0400

    change hero image height to 500px

--- src/components/Hero/element.jsx
+++ src/components/Hero/element.jsx
@@ -15,7 +15,7 @@ export const Image = styled.img`
 `
 export const Banner = styled.div`
   width: 100%;
-  height: 400px;
+  height: 500px;
   
   border-top: 8px solid rgba(245, 245, 245, 0.685);
   border-bottom: 8px solid rgba(245, 245, 245, 0.685);
