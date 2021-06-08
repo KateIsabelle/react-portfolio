0a82682dc66bf3bc67925c6d06d0306736cf1740
Kate Davis <kate.davis887@gmail.com>
2021-06-08 19:05:44 -0400

    add background image for Hero component

---
+++ src/components/Hero/element.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  transform: translateX(-100%);
+  -webkit-transform: translateX(-100%);
+`
+
+export const Image = styled.img`
+  height: auto;
+  width: 100%;
+  overflow: hidden;
+  border-top: 8px solid rgba(245, 245, 245, 0.685);
+  border-bottom: 8px solid rgba(245, 245, 245, 0.685);
+  box-shadow: 2px;
+`
+export const Banner = styled.div`
+  width: 100%;
+  height: 400px;
+  
+  border-top: 8px solid rgba(245, 245, 245, 0.685);
+  border-bottom: 8px solid rgba(245, 245, 245, 0.685);
+  box-shadow: 2px;
+  background-image: url("https://res.cloudinary.com/davik/image/upload/v1623193254/react-portfolio/background2_js5ygh.png");
+  background-size: cover;
+`
+
+// https://res.cloudinary.com/davik/image/upload/v1623193254/react-portfolio/background2_js5ygh.png
